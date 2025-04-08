66a88ddd28c33b035bf6edfb66cab0c3934e13d0
Rajesh Verma <rajesh_bhilawan@rediffmail.com>
2025-04-08 20:07:41 +0530

    static class

---
+++ Interview/Others/StaticClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview.Others
+{
+    static class StaticClass
+    {
+         static StaticClass()
+        {
+
+        }
+
+        private static void ToDo()
+        {
+
+        }
+        //public StaticClass()
+        //{
+
+        //}
+
+    }
+}
