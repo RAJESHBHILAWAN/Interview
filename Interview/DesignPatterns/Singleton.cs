e08b6c2db13b1fe4622a4e9b056871c81b285eea
Rajesh Verma <rajesh_bhilawan@rediffmail.com>
2025-04-08 19:16:50 +0530

    singleton changes

--- Interview/DesignPatterns/Singleton.cs
+++ Interview/DesignPatterns/Singleton.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace InterviewPrep.DesignPatterns
 {
@@ -42,21 +44,28 @@ namespace InterviewPrep.DesignPatterns
             instanceCounter++;
             Console.WriteLine($"No of instances created are : {instanceCounter}");
         }
-
         public static Singleton GetInstance() {
+            
 
-            lock (instanceLock) { 
-               
-              if (instance == null)
+            //This is called Double checked locking mechanism, 
+            //first, we will check whether the instance is created or not.
+            //If not then only we will synchronize the method and create the instance. 
+            //It will drastically improve the performance of the application.Performing lock is 
+            //heavy.So to avoid the lock first we need to check the null value.This is also 
+            //thread safe and it is the best way to achieve the best performance. Please have 
+            //a look at the following code.
+            if (instance == null)
+            {
+                lock (instanceLock)
                 {
-                    instance = new Singleton();
-                   
+                    if (instance == null)
+                    {
+                        instance = new Singleton();
+                    }
                 }
             }
-
-
-         return instance;
-        }
+            return instance;
+            }
 
         public void WriteMessage(string message) { 
             Console.WriteLine($"Message from singleton: {message} at instance: { instanceCounter }" );
