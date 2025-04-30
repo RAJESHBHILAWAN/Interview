ea0ea9dfa52ce5aec71388751bb01e818d077a4a
Rajesh Verma <rajesh_bhilawan@rediffmail.com>
2025-04-30 11:14:44 +0530

    Entity Framework

---
+++ Interview/EntityFrameword/EntityDB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview.EntityFrameword
+{
+    internal class EntityDB : DbContext
+    {
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        public EntityDB() : base("name=WebApiDbEntities")
+        {
+
+
+        }
+
+    }
+}
