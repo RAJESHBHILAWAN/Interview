2a37b7f209a67dfac48985f9cf3d5f59fe984d72
Rajesh Verma <rajesh_bhilawan@rediffmail.com>
2025-05-01 10:38:42 +0530

    SealedMethods

---
+++ Interview/Others/SealedMethods.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview.Others
+{
+    internal class SealedMethods
+    {
+        public SealedMethods() { }
+    }
+
+   public class BaseClass
+    {
+        public virtual void MyMethod()
+        {
+            // Base class implementation
+        }
+    }
+    public class DerivedClass : BaseClass
+    {
+        // This method is sealed, meaning it can’t be overridden again
+        public sealed override void MyMethod()
+        {
+            // Specific implementation for DerivedClass
+        }
+    }
+
+ public class AnotherDerivedClass : DerivedClass
+{
+        // This will cause a compile-time error because MyMethod is sealed in DerivedClass
+        //public override void MyMethod()
+        //{
+        //    // This is not allowed
+        //}
+    }
+
+
+
+
+
+
+    ////////////////
+
+
+}
