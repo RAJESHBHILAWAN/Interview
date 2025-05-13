ab072b8a35db094c566fdee4d98c084c0c507586
Rajesh Verma <rajesh_bhilawan@rediffmail.com>
2025-05-13 14:23:32 +0530

    Events

--- Interview/Others/AnonymousMethods.cs
+++ Interview/Others/AnonymousMethods.cs
@@ -81,17 +81,7 @@ namespace InterviewPrep.Others
 
         }
 
-        private void MyEvent()
-        {
-            var account1 = new AccountManagement();
-            var person = new Person(account1) { Name = "John Doe" };
-            account1.Winthdraw(person);
-            var account2 = new AccountManagement();
-            var person2 = new Person(account2) { Name = "Justin Phillip" };
-            account2.Winthdraw(person2);
-            Console.ReadLine();
-
-        }
+      
 
 
 
@@ -100,32 +90,5 @@ namespace InterviewPrep.Others
 
 
 
-    public class Person
-    {
-        public string Name { get; set; }
-        private AccountManagement _account;
-        public Person(AccountManagement account)
-        {
-            _account = account;
-            account.Deposithandler += account.DeductMoney; //Subscribe to Deduct Money Event
-        }
-    }
-
-    public class AccountManagement
-    {
-        public delegate void DepositHandler(object sender, string eventArgs);
-        public event DepositHandler Deposithandler ;
-
-        public void DeductMoney(object sender, string eventArgs)
-        {
-            Console.WriteLine(eventArgs + ": Money has been deducted from your account\n");
-        }
-
-        public void Winthdraw(Person person)
-        {
-            Console.WriteLine(person.Name + ": You withdraw money");
-            Deposithandler(this, person.Name);
-        }
-    }
-
+ 
 }
---
+++ Interview/Others/myEvents.cs
@@ -0,0 +1,58 @@
+﻿using InterviewPrep.Others;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview.Others
+{
+    internal class myEvents
+    {
+        public myEvents() {
+            MyEvent();
+        }
+
+        private void MyEvent()
+        {
+            var account1 = new AccountManagement();
+            var person = new Person(account1) { Name = "John Doe" };
+            account1.Winthdraw(person);
+            var account2 = new AccountManagement();
+            var person2 = new Person(account2) { Name = "Justin Phillip" };
+            account2.Winthdraw(person2);
+            Console.ReadLine();
+
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        private AccountManagement _account;
+        public Person(AccountManagement account)
+        {
+            _account = account;
+            account.Deposithandler += account.DeductMoney; //Subscribe to Deduct Money Event
+        }
+    }
+
+    public class AccountManagement
+    {
+        public delegate void DepositHandler(object sender, string eventArgs);
+        public event DepositHandler Deposithandler;
+
+        public void DeductMoney(object sender, string eventArgs)
+        {
+            Console.WriteLine(eventArgs + ": Money has been deducted from your account\n");
+        }
+
+        public void Winthdraw(Person person)
+        {
+            Console.WriteLine(person.Name + ": You withdraw money");
+            Deposithandler(this, person.Name);
+        }
+    }
+
+
+}
